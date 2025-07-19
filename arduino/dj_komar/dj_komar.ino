3983ebc20edac1380a6da8f62b1a07df43f5abf9
mirai-ux <raaszard666@gmail.com>
2025-07-19 22:24:15 +0200

    feat!: arduino code now supports multiple pots
    BREAKING(bug): doesn't work at all with the rust code for now

--- arduino/dj_komar/dj_komar.ino
+++ arduino/dj_komar/dj_komar.ino
@@ -1,15 +1,27 @@
-const int POT_COUNT = 9;
-const int analogIndex[POT_COUNT] = {A0, A1, A2, A3, A4, A5, A6, A7};
+const int POT_COUNT = 6;
+const int analogIndex[POT_COUNT] = {A0, A1, A2, A3, A4, A5};
 
 int analogIndexValue[POT_COUNT];
+int lastAnalogValue[POT_COUNT];
+const int threshold = 10;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  for (int i = 0; i < POT_COUNT; i++) {
+    lastAnalogValue[i] = analogRead(analogIndex[i]);
+  }
 }
 
 void loop() {
-  analogIndexValue[0] = analogRead(analogIndex[0]);
-  Serial.println(analogIndexValue[0]);
-  delay(300);
+  for (int i = 0; i < POT_COUNT; i++) {
+    analogIndexValue[i] = analogRead(analogIndex[i]);
+    if (abs(analogIndexValue[i] - lastAnalogValue[i]) > threshold) {
+      String label = "A" + String(analogIndex[i] - A0);
+      Serial.print(label);
+      Serial.print("_");
+      Serial.println(analogIndexValue[i]);
+      lastAnalogValue[i] = analogIndexValue[i];
+    }
+  }
+  delay(30);
 }
